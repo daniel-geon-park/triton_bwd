725d8afdf36f79570564c1ad37b669b40972d727
Geon Park <mujjingun@gmail.com>
2025-07-05 14:25:22 +0900

    wip

--- src/triton_bwd/make_ast.py
+++ src/triton_bwd/make_ast.py
@@ -1,6 +1,7 @@
 import ast
 import inspect
 import re
+from contextlib import contextmanager
 from types import FunctionType
 from typing import Any, Callable, Dict, List, NewType, Optional, Union
 
@@ -8,6 +9,58 @@ import numpy as np
 import sympy
 
 from triton_bwd.constexpr import Constexpr
+
+
+class SympyIndexing(sympy.Function):
+    @classmethod
+    def eval(cls, array, index):
+        pass
+
+    def _sympystr(self, printer):
+        array, index = self.args
+        if isinstance(index, sympy.Tuple):
+            index_str = ", ".join(printer.doprint(i) for i in index)
+        else:
+            index_str = printer.doprint(index)
+        return printer.doprint(array) + "[" + index_str + "]"
+
+
+def broadcasat_shapes(shape1: sympy.Tuple, shape2: sympy.Tuple):
+    """Broadcast two shapes together."""
+    shape1, shape2 = shape1.args, shape2.args
+    len1, len2 = len(shape1), len(shape2)
+    if len1 < len2:
+        shape1 = sympy.Tuple(*([1] * (len2 - len1)) + list(shape1))
+    elif len2 < len1:
+        shape2 = sympy.Tuple(*([1] * (len1 - len2)) + list(shape2))
+
+    # Check for incompatible dimensions
+    for idim, (dim1, dim2) in enumerate(zip(shape1, shape2)):
+        if dim1 != 1 and dim2 != 1 and dim1 != dim2:
+            raise ValueError(f"Incompatible {idim}th dimensions: {dim1} and {dim2}")
+
+    return sympy.Tuple(*[sympy.Max(dim1, dim2) for dim1, dim2 in zip(shape1, shape2)])
+
+
+class SympyShape(sympy.Function):
+    @classmethod
+    def eval(cls, array):
+        if isinstance(array, sympy.Number):
+            return sympy.Tuple()
+        if isinstance(array, sympy.Symbol):
+            return sympy.Tuple()  # scalar
+        if isinstance(array, sympy.IndexedBase):
+            return array.shape
+        if isinstance(array, Union[sympy.Add, sympy.Mul]):
+            a, b = array.as_two_terms()
+            a_shape, b_shape = SympyShape(a), SympyShape(b)
+            shape = broadcasat_shapes(a_shape, b_shape)
+            return sympy.Tuple(*shape)
+        if isinstance(array, SympyIndexing):
+            # TODO: handle slices and other indexing
+            return sympy.Tuple()
+        # TODO: implement other array operations
+        raise ValueError(f"Unsupported type for shape: {type(array)}")
 
 
 class ArraySpec:
@@ -19,7 +72,11 @@ class ArraySpec:
 
     def symbol(self) -> sympy.Symbol:
         assert self.name is not None
-        return sympy.symbols(self.name, cls=sympy.IndexedBase)
+        shape = tuple(sympy.symbols(dim, integer=True) for dim in self.dims)
+        return sympy.IndexedBase(
+            self.name,
+            shape=shape,
+        )
 
 
 class IntSpec:
@@ -44,16 +101,26 @@ class ForLoop:
         index_begin: int,
         index_end: int,
         index_step: int,
+        declarations: Dict[str, sympy.Basic],
         statements: List["AbstractNode"],
     ):
         self.index_var = index_var
         self.index_begin = index_begin
         self.index_end = index_end
         self.index_step = index_step
+        self.declarations = declarations
         self.statements = statements
 
     def __repr__(self):
         stmt_reprs = []
+        for name, decl in self.declarations.items():
+            shape = SympyShape(decl)
+            if shape == ():
+                stmt_reprs.append(f"let {name}: scalar")
+            else:
+                stmt_reprs.append(
+                    f"let {name}: array({', '.join(map(str, shape.args))})"
+                )
         for stmt in self.statements:
             stmt_repr = repr(stmt)
             stmt_reprs.extend(stmt_repr.split("\n"))
@@ -64,7 +131,7 @@ class ForLoop:
 
 
 class Assignment:
-    def __init__(self, target: sympy.Expr, value: sympy.Expr):
+    def __init__(self, target: sympy.Basic, value: sympy.Basic):
         self.target = target
         self.value = value
 
@@ -139,10 +206,11 @@ class OptimizableFunction:
         source = inspect.getsource(self.func)
         tree = ast.parse(source)
 
+        args = {name: spec.symbol() for name, spec in self.arg_specs.items()}
         visitor = NodeVisitor(
             call_stack=[self.func.__name__],
             func_globals=self.func.__globals__,
-            arg_specs=self.arg_specs,
+            args=args,
         )
         self.abstract_tree = visitor.visit(tree)
 
@@ -181,9 +249,9 @@ def _apply_binary_method(op, lhs, rhs):
     op_name = _method_name_for_bin_op.get(type(op))
     rev_op_name = re.sub(r"__(.*)__", r"__r\1__", op_name)
     assert op_name is not None
-    if isinstance(lhs, sympy.Expr):
+    if isinstance(lhs, sympy.Basic):
         return getattr(lhs, op_name)(rhs)
-    if isinstance(rhs, sympy.Expr):
+    if isinstance(rhs, sympy.Basic):
         return getattr(rhs, rev_op_name)(lhs)
     return getattr(Constexpr(lhs), op_name)(Constexpr(rhs)).value
 
@@ -193,12 +261,12 @@ class NodeVisitor(ast.NodeVisitor):
         self,
         call_stack: list[str],
         func_globals: Dict[str, Any],
-        arg_specs: Dict[str, ArgSpec],
+        args: Dict[str, sympy.Basic],
     ):
         super().__init__()
         self.call_stack = call_stack
         self.func_globals = func_globals
-        self.arg_specs = arg_specs
+        self.args = args
         self.locals = {}
 
     def set_local(self, name, value):
@@ -207,8 +275,15 @@ class NodeVisitor(ast.NodeVisitor):
     def visit(self, node: Any):
         try:
             value = super().visit(node)
-        except RuntimeError as e:
-            raise ValueError(f"{self.call_stack[-1]}:{node.lineno}: {e}")
+            if isinstance(node, ast.expr):
+                if not isinstance(value, sympy.Basic):
+                    raise ValueError(
+                        f"Expression {node} must return a sympy type, "
+                        f"but got {type(value)}: {value}"
+                    )
+        except (ValueError, NotImplementedError) as e:
+            print(f"{self.call_stack[-1]}:{getattr(node, 'lineno', None)}: {type(e)}")
+            raise e
         if value is NotImplemented:
             raise NotImplementedError(f"{node} returned NotImplemented")
         return value
@@ -218,7 +293,7 @@ class NodeVisitor(ast.NodeVisitor):
 
     def visit_FunctionDef(self, node) -> AbstractNode:
         nodes = self.visit_compound_statement(node.body)
-        return AbstractNode(ForLoop("__root_index", 0, 1, 1, nodes))
+        return AbstractNode(ForLoop("__root_index", 0, 1, 1, {}, nodes))
 
     def visit_Return(self, node):
         raise NotImplementedError
@@ -228,46 +303,63 @@ class NodeVisitor(ast.NodeVisitor):
 
     def visit_Assign(self, node):
         if len(node.targets) != 1:
-            raise ValueError(
-                f"{self.call_stack[-1]}:{node.lineno}: "
-                "Only single assignment is supported."
-            )
+            raise ValueError("Only single assignment is supported.")
         value = self.visit(node.value)
+        value_shape = SympyShape(value)
         target_node = node.targets[0]
         if isinstance(target_node, ast.Name):
-            target = sympy.symbols(target_node.id)
-            self.set_local(target_node.id, target)
+            if value_shape.args == ():
+                target = sympy.symbols(target_node.id)
+            else:
+                target = sympy.IndexedBase(
+                    target_node.id,
+                    shape=value_shape.args,
+                )
+            if target_node.id not in self.locals:
+                self.set_local(target_node.id, target)
         else:
             target = self.visit(target_node)
         return Assignment(target, value)
 
     def visit_AugAssign(self, node):
         target = self.visit(node.target)
+        target_shape = SympyShape(target)
         value = self.visit(node.value)
+        value_shape = SympyShape(value)
+        if target_shape.args != value_shape.args:
+            raise ValueError(
+                f"Shape mismatch in augmented assignment: "
+                f"{target_shape=} vs {value_shape=}"
+            )
         result = _apply_binary_method(node.op, target, value)
         return Assignment(target, result)
 
     def visit_AnnAssign(self, node):
         raise NotImplementedError
 
+    def scope(self, extra_locals: Dict[str, sympy.Basic] = None):
+        if extra_locals is None:
+            extra_locals = {}
+        block = NodeVisitor(
+            call_stack=self.call_stack,
+            func_globals=self.func_globals,
+            args=self.args | self.locals,
+        )
+        for name, value in extra_locals.items():
+            block.set_local(name, value)
+        return block
+
     def visit_For(self, node) -> AbstractNode:
         if not isinstance(node.target, ast.Name):
-            raise ValueError(
-                f"{self.call_stack[-1]}:{node.lineno}: "
-                "For loop index must be a variable name."
-            )
+            raise ValueError("For loop index must be a variable name.")
 
         if not isinstance(node.iter, ast.Call):
-            raise ValueError(
-                f"{self.call_stack[-1]}:{node.lineno}: "
-                "For loop iteration must be a range."
-            )
-        IteratorClass = self.visit(node.iter.func)
+            raise ValueError("For loop iteration must be a range.")
+        if not isinstance(node.iter.func, ast.Name):
+            raise ValueError("For loop iteration must be a range.")
+        IteratorClass = self.dereference_name(node.iter.func.id)
         if IteratorClass is not range:
-            raise ValueError(
-                f"{self.call_stack[-1]}:{node.lineno}: "
-                "For loop iteration must be a range."
-            )
+            raise ValueError("For loop iteration must be a range.")
 
         iter_args = [self.visit(arg) for arg in node.iter.args]
         if len(iter_args) == 1:
@@ -285,17 +377,21 @@ class NodeVisitor(ast.NodeVisitor):
         loop_var_name = node.target.id
 
         if loop_var_name in self.locals:
-            raise ValueError(
-                f"{self.call_stack[-1]}:{node.lineno}: "
-                f"Loop variable {loop_var_name} already defined."
-            )
-        self.set_local(loop_var_name, sympy.symbols(loop_var_name))
+            raise ValueError(f"Loop variable {loop_var_name} already defined.")
 
-        statements = self.visit_compound_statement(node.body)
+        scope = self.scope(
+            extra_locals={loop_var_name: sympy.symbols(loop_var_name, integer=True)}
+        )
+        statements = scope.visit_compound_statement(node.body)
 
-        del self.locals[loop_var_name]
+        declarations = {}
+        for name, target in scope.locals.items():
+            if name != loop_var_name:
+                declarations[name] = target
 
-        return AbstractNode(ForLoop(loop_var_name, begin, end, step, statements))
+        return AbstractNode(
+            ForLoop(loop_var_name, begin, end, step, declarations, statements)
+        )
 
     def visit_compound_statement(self, stmts) -> List[AbstractNode]:
         # Ensure that stmts is iterable
@@ -351,12 +447,11 @@ class NodeVisitor(ast.NodeVisitor):
     def visit_Continue(self, node):
         raise NotImplementedError
 
+    # Expressions
+
     def visit_BoolOp(self, node):
         if len(node.values) != 2:
-            raise ValueError(
-                f"{self.call_stack[-1]}:{node.lineno}: "
-                "Only binary boolean operations are supported."
-            )
+            raise ValueError("Only binary boolean operations are supported.")
         left = self.visit(node.values[0])
         right = self.visit(node.values[1])
         if isinstance(node.op, ast.And):
@@ -433,7 +528,13 @@ class NodeVisitor(ast.NodeVisitor):
         raise NotImplementedError
 
     def visit_Constant(self, node):
-        return node.value
+        if isinstance(node.value, Union[int, float]):
+            return sympy.Number(node.value)
+        if isinstance(node.value, bool):
+            return sympy.true if node.value else sympy.false
+        raise ValueError(
+            f"Unsupported constant type: {type(node.value)} with value {node.value}"
+        )
 
     def visit_Attribute(self, node):
         raise NotImplementedError
@@ -441,7 +542,7 @@ class NodeVisitor(ast.NodeVisitor):
     def visit_Subscript(self, node):
         value = self.visit(node.value)
         index = self.visit(node.slice)
-        return value[index]
+        return SympyIndexing(value, index)
 
     def visit_Starred(self, node):
         raise NotImplementedError
@@ -450,12 +551,11 @@ class NodeVisitor(ast.NodeVisitor):
         return self.dereference_name(node.id)
 
     def visit_List(self, node):
-        args = [self.visit(x) for x in node.elts]
-        return args
+        raise NotImplementedError
 
     def visit_Tuple(self, node):
         args = [self.visit(x) for x in node.elts]
-        return tuple(args)
+        return sympy.Tuple(*args)
 
     def visit_Slice(self, node):
         lower = self.visit(node.lower) if node.lower is not None else None
@@ -463,7 +563,7 @@ class NodeVisitor(ast.NodeVisitor):
         step = self.visit(node.step) if node.step is not None else None
         return slice(lower, upper, step)
 
-    def dereference_name(self, name, absent=None) -> sympy.Expr:
+    def dereference_name(self, name, absent=None) -> sympy.Basic:
         error_if_absent = False
         if absent is None:
             error_if_absent = True
@@ -472,8 +572,7 @@ class NodeVisitor(ast.NodeVisitor):
         if val is absent:
             val = self.locals.get(name, absent)
         if val is absent:
-            if name in self.arg_specs:
-                val = self.arg_specs[name].symbol()
+            val = self.args.get(name, absent)
         if val is absent:
             val = builtin_namespace.get(name, absent)
         if error_if_absent and val is absent:
